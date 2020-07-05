2bcfcad27c1049737fc4a2b7ecf616a9d5fb4afb
Jake Duncan <jakecduncan@gmail.com>
2020-07-04 18:28:11 -0600

    post body

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,15 @@ import Bio from "../components/bio";
 import Post from "../components/post";
 import QuoteSection from "../components/quoteSection";
 import posts from "../post-config";
+import { useRouter } from "next/router";
 
 export default function Home() {
+  const router = useRouter();
+
+  let handleGoHomeClick = () => {
+    router.push("/");
+  };
+
   return (
     <div className="container">
       <Head>
@@ -12,7 +19,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <h1>jakeduncan.dev</h1>
+        <h1 onClick={handleGoHomeClick}>jakeduncan.dev</h1>
         <Bio className="post" />
         <QuoteSection className="post" />
         {posts.map((postDetail) => {
@@ -32,82 +39,6 @@ export default function Home() {
           <a href="mailto:jakecduncan@gmail.com">jakecduncan@gmail.com</a>
         </div>
       </footer>
-
-      <style jsx>{`
-        main {
-          padding: 2rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          width: 80%;
-          height: 80%;
-          display: flex;
-          justify-content: flex-start;
-          align-items: flex-start;
-        }
-
-        footer {
-          width: 80%;
-          height: 50px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: flex-start;
-          align-items: center;
-        }
-
-        .footer-text {
-          color: hsla(0, 0%, 100%, 0.88);
-          margin: 0 10px;
-        }
-      `}</style>
-
-      <style jsx global>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        h1 {
-          margin: 10px 0 0 0;
-          color: white;
-        }
-
-        h6 {
-          margin: 10px 0 0 0;
-          color: hsla(0, 0%, 100%, 0.88);
-        }
-
-        a {
-          color: gold;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-          background: #282c35;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
     </div>
   );
 }
--- pages/posts/[post].js
+++ pages/posts/[post].js
@@ -6,9 +6,13 @@ import posts from "../../post-config";
 
 export default function PostWrapper() {
   const router = useRouter();
-  const { post } = router.query;
   const [DynamicComponent, setDynamicComponent] = useState(null);
 
+  let handleGoHomeClick = () => {
+    router.push("/");
+  };
+
+  const { post } = router.query;
   useEffect(() => {
     setDynamicComponent(dynamic(() => import(`../../markdown/${post}`)));
   }, [post]);
@@ -20,8 +24,10 @@ export default function PostWrapper() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <h1>jakeduncan.dev</h1>
-        {DynamicComponent && <DynamicComponent />}
+        <h1 onClick={handleGoHomeClick}>jakeduncan.dev</h1>
+        {DynamicComponent && (
+          <DynamicComponent className="postBody" post={post} />
+        )}
       </main>
 
       <footer>
@@ -30,34 +36,6 @@ export default function PostWrapper() {
           <a href="mailto:jakecduncan@gmail.com">jakecduncan@gmail.com</a>
         </div>
       </footer>
-
-      <style jsx>{`
-        main {
-          padding: 2rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          width: 80%;
-          height: 80%;
-          display: flex;
-          justify-content: flex-start;
-          align-items: flex-start;
-        }
-
-        footer {
-          width: 80%;
-          height: 50px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: flex-start;
-          align-items: center;
-        }
-
-        .footer-text {
-          color: hsla(0, 0%, 100%, 0.88);
-          margin: 0 10px;
-        }
-      `}</style>
     </div>
   );
 }
