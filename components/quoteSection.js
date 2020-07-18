dde0f28342b171225e706300a493d247fb45fd4f
Jake Duncan <jakecduncan@gmail.com>
2020-07-18 13:05:32 -0600

    Remove unnecessary state / effect.

--- components/quoteSection.js
+++ components/quoteSection.js
@@ -2,24 +2,18 @@ import { quotes } from "../site-config";
 import { useState, useEffect } from "react";
 
 export default function QuoteSection({ className }) {
-  const [quoteToShowIndex, setQuoteToShowIndex] = useState(0);
+  const millisecondsInDay = 24 * 60 * 60 * 1000;
+  let comparisonDate = new Date("2020", "6", "9");
+  let currentDate = new Date();
 
-  useEffect(() => {
-    const millisecondsInDay = 24 * 60 * 60 * 1000;
-    let comparisonDate = new Date("2020", "6", "9");
-    let currentDate = new Date();
+  // get difference between dates in milliseconds then convert back to days
+  let quoteToShowIndex = Math.round(
+    Math.abs((currentDate - comparisonDate) / millisecondsInDay)
+  );
 
-    // get difference between dates in milliseconds then convert back to days
-    let quoteToShowIndexTemp = Math.round(
-      Math.abs((currentDate - comparisonDate) / millisecondsInDay)
-    );
-
-    while (quoteToShowIndexTemp > quotes.length - 1) {
-      quoteToShowIndexTemp -= quotes.length;
-    }
-
-    setQuoteToShowIndex(quoteToShowIndexTemp);
-  }, []);
+  while (quoteToShowIndex > quotes.length - 1) {
+    quoteToShowIndex -= quotes.length;
+  }
 
   return <div className={className}>{quotes[quoteToShowIndex]}</div>;
 }
